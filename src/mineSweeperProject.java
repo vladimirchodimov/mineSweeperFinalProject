adc455a6695c15d273ae79e2f04e98d60a523a9f
vladimirchodimov <vladimir_dimov@abv.bg>
2019-01-13 20:04:05 +0200

    sleepy

--- src/mineSweeperProject.java
+++ src/mineSweeperProject.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 public class mineSweeperProject {
     public static void main(String[] args) {
 
-//        char field[] = {'[', '?', ']'};
+        Scanner input = new Scanner(System.in);
+
         int row = 0;   //use only to change the row in if statement
         char blanck = '?';        // encrypt the information about the field - 1 2 3
 
-        int field[] = new int[100];   //array for taking the value of the field cordinate
+        int field[] = new int[100];   //array for taking the value of the field cordinates
         Random bombGenerator = new Random();
-        int field1 = 0;    //gadering the numbers from 1 to 3 generated from the random generator
+        int field1 = 0;    //gadering the numbers from 1 to 2 generated from the random generator
         int x; // system input
         int numberOfBombs = 0;
 
@@ -18,8 +19,8 @@ public class mineSweeperProject {
             System.out.print("[ " + i + " ]");
         }
         System.out.println("");
-        for (int i = 0; i < 100; i++) {
-            field1 = 1 + bombGenerator.nextInt(4);
+        for (int i = 0; i < field.length; i++) {
+            field1 = 1 + bombGenerator.nextInt(4); // the idea is to start from 1
             row++;
 
             if (field1 == 1) {
@@ -36,25 +37,36 @@ public class mineSweeperProject {
 
             field[i] = field1;
 
-
             if (field[i] == 1 || field[i] == 2) {
 
                 System.out.print("[");
-                System.out.print(" " + field[i] + " ");  // blanck or field[i]
+                System.out.print(" " + blanck + " ");  // blanck or field[i]
+                System.out.print("]");
+            }else{
+                System.out.print("[");
+                System.out.print(" " + "0" + " ");  // blanck or field[i]
                 System.out.print("]");
             }
             if (row % 10 == 0) {
                 System.out.println("[" + row / 10 + "]");
-
             }
+
         }
-        Scanner input = new Scanner(System.in);
+
 
         do {
-            System.out.println("Въведете стойност за от 1 до 9 позиция по хоризонтала и вертикала");
+
+
+            System.out.println("Въведете стойност за  позиция по хоризонтала и вертикала от 1 до 100, " +
+                    "ако желаете да излезете от програмата натиснете 0 или число над 101");
             x = input.nextInt();
+            if (x > 101) {
+                x = 0;
+            }
             checkForMines(x, field);
-        } while (x != 101 || x != 0);
+printTableAfetCheck(field, blanck);
+
+        } while (x > 0);
 
     }
 
@@ -63,16 +75,44 @@ public class mineSweeperProject {
         if (x < 101 && x > 0) {
             x -= 1;
             if (field[x] == 1) {
-                System.out.println("it is a bomb");
+                System.out.println("It is a bomb !");
             } else if (field[x] == 2) {
-                System.out.println("go on");
+                System.out.println("Go on");
+                field[x] = 0;
             }
 
         } else {
             System.out.println("you have quit the game");
         }
 
+    }
+    public static void printTableAfetCheck(int[] field, int blanck){
+        int row3=0;
+        for (int i = 1; i <= 10; i++) {
+            System.out.print("[ " + i + " ]");
+        }
+        System.out.println("");
+        for (int i = 0; i < field.length; i++) {
+            row3++;
+            if (field[i] == 1 || field[i] == 2) {
+                if (field[i]==2){
+                    System.out.print("[");
+                    System.out.print(" " + "0" + " ");  // blanck or field[i]
+                    System.out.print("]");
+                }else {
 
+                    System.out.print("[");
+                    System.out.print(" " + blanck + " ");  // blanck or field[i]
+                    System.out.print("]");
+                }
+            }
+
+
+            if (row3 % 10 == 0) {
+                System.out.println("[" + row3 / 10 + "]");
+            }
+
+        }
     }
 
 }
