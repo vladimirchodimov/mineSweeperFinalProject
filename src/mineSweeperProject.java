09c381a486cb4be752f8acf391df35541fc195c1
vladimirchodimov <vladimir_dimov@abv.bg>
2019-01-15 01:33:11 +0200

    it remains only the flags issue, the timer is set

--- src/mineSweeperProject.java
+++ src/mineSweeperProject.java
@@ -1,18 +1,29 @@
+
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class mineSweeperProject {
+
+    private long startTime = 0;
+    private long stopTime = 0;
+    private boolean running = false;
+
     public static void main(String[] args) {
+
+        StopWatch s = new StopWatch();
+        s.start();
+        long timeForChange= s.getElapsedTime();
 
         Scanner input = new Scanner(System.in);
 //----------------------------------------------------------------------------------------------------- initialize
         int row = 0;   //use only to change the row in if statement
 
         int field[] = new int[100];   //array for taking the value of the field cordinates
-
+        int[] flags = new int[10];
         Random bombGenerator = new Random();
         int field1 = 0;    //gadering the numbers from 1 to 2 generated from the random generator
-        int x; // system input
+        int x; // system keyboard input
         int numberOfBombs = 0;
 
 //------------------------------------------------------------------------------------------------------ initialize
@@ -32,7 +43,7 @@ public class mineSweeperProject {
                 numberOfBombs++;
 
             }
-            if (numberOfBombs > 20) {
+            if (numberOfBombs > 10) {
                 field1 = 2;
             }
             if (field1 == 3 || field1 == 4) {
@@ -55,12 +66,24 @@ public class mineSweeperProject {
             }
 
         }
-        flagsForBombs(field);
+        flagsForBombs(field, flags);
+
 //-------------------------------------------------------------------------generating bombs and field
 // and placing them randomly
 
 
 // -------------------------------------------------------------------------refreshing the field with the changes
+        System.out.println("The flag of the Bombs");
+        System.out.println("_____________________________________________________");
+        s.getTimeForAll();
+
+
+        for (int j = 0; j < flags.length; j++) {
+            System.out.print("~ " + flags[j] + " ~");
+
+        }
+        System.out.println("");
+        System.out.println("_____________________________________________________");
         do {
 
             System.out.println("Въведете стойност за  позиция по хоризонтала и вертикала от 1 до 100, " +
@@ -73,8 +96,18 @@ public class mineSweeperProject {
             checkForMines(x, field);
             printTableAfetCheck(field, x);
 
-        } while (x > 0);
+            System.out.println("The flag of the Bombs");
+            System.out.println("_____________________________________________________");
+            s.getTimeForAll();
+            for (int j = 0; j < flags.length; j++) {
+                System.out.print("~ " + flags[j] + " ~");
 
+            }
+            System.out.println("");
+            System.out.println("_____________________________________________________");
+
+        } while (x > 0);
+        s.stop();
     }
 
     //-------------------------------------------------------------------------refreshing the field with the changes
@@ -133,8 +166,8 @@ public class mineSweeperProject {
 
     //------------------------------------------------------------ gives a value for the flags
 
-    public static void flagsForBombs(int[] field) {
-        int[] flags = new int[10];
+    public static int[] flagsForBombs(int[] field, int[] flags) {
+
 
         for (int i = 0; i < field.length; i++) {
 
@@ -190,16 +223,66 @@ public class mineSweeperProject {
 
 
         }
-        System.out.println("The flag of the Bombs");
-        System.out.println("_____________________________________________________");
-        for (int j = 0; j < flags.length; j++) {
-            System.out.print("~ " + flags[j] + " ~");
 
-        }
-        System.out.println("");
-        System.out.println("_____________________________________________________");
-
+        return flags;
     }
-    //---------------------------------------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------stuff for timer
+
+
+
+    public void start() {
+        this.startTime = System.currentTimeMillis();
+        this.running = true;
+    }
+
+
+    public void stop() {
+        this.stopTime = System.currentTimeMillis();
+        this.running = false;
+    }
+
+
+    //elaspsed time in milliseconds
+    public long getElapsedTime() {
+        long elapsed;
+        if (running) {
+            elapsed = (System.currentTimeMillis() - startTime);
+        } else {
+            elapsed = (stopTime - startTime);
+        }
+        return elapsed;
+    }
+
+
+    //elaspsed time in seconds
+    public long getElapsedTimeSecs() {
+        long elapsed;
+        if (running) {
+            elapsed = ((System.currentTimeMillis() - startTime) / 1000);
+        } else {
+            elapsed = ((stopTime - startTime) / 1000);
+        }
+        return elapsed;
+    }
+
+
+    public void getTimeForAll() {
+
+        long milliTime = getElapsedTime();
+        int[] out = new int[]{0, 0, 0, 0};
+        out[0] = (int) (milliTime / 3600000);
+        out[1] = (int) (milliTime / 60000) % 60;
+        out[2] = (int) (milliTime / 1000) % 60;
+        out[3] = (int) (milliTime) % 1000;
+
+
+        System.out.print("Time past : " + out[0] + ":" + out[1] + ":" + out[2] + "." + out[3]);
+
+
+        System.out.println(" ");
+
+        //------------------------------------------------------------------stuff for timer
+    }
 
 }
+
--- src/testsForMineSweeper.java
+++
@@ -1,5 +0,0 @@
-public class testsForMineSweeper {
-    public static void main(String[] args) {
-
-    }
-}
